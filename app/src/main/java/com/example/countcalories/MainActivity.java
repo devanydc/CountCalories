8cc4c9d7753b55d0424ca92875403763dd2e76d9
Abyan <harlem.minion09@gmail.com>
2019-11-19 09:57:13 +0700

    add navigation

--- app/src/main/java/com/example/countcalories/MainActivity.java
+++ app/src/main/java/com/example/countcalories/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.countcalories;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.countcalories.fragments.FoodFragment;
+import com.example.countcalories.fragments.HomeFragment;
+import com.example.countcalories.fragments.TrainingFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new FoodFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_food:
+                fragment = new HomeFragment();
+                break;
+            case R.id.action_movie:
+                fragment = new FoodFragment();
+                break;
+            case R.id.action_discount:
+                fragment = new TrainingFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
